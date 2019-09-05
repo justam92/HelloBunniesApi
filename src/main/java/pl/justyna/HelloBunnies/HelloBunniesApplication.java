722ea9d31a6ccb89a3fcdf1c3a780e4641f482aa
justam92 <31111330+justam92@users.noreply.github.com>
2019-09-05 16:02:24 +0200

    Initial app with dependencies and db configuration

---
+++ src/main/java/pl/justyna/HelloBunnies/HelloBunniesApplication.java
@@ -0,0 +1,13 @@
+package pl.justyna.HelloBunnies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBunniesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBunniesApplication.class, args);
+	}
+
+}
